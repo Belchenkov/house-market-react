45a9b540b23f138b798936cf2c275ea0a0a96609
Алексей Бельченков <u608110@gmail.com>
2022-03-22 19:16:43 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>House Market Place</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBsq_Q49dD15AV-_N2ESB-xkPOpGVQZlnc",
+    authDomain: "house-marketplace-react-c07d3.firebaseapp.com",
+    projectId: "house-marketplace-react-c07d3",
+    storageBucket: "house-marketplace-react-c07d3.appspot.com",
+    messagingSenderId: "387218769167",
+    appId: "1:387218769167:web:ad0abe974d851a30d52a7a"
+};
+
+// Initialize Firebase
+export const db = getFirestore();
