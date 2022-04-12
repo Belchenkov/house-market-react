da8fa78cf0bbe0db841a5bf27b4e5cf71669772f
Алексей Бельченков <u608110@gmail.com>
2022-04-12 19:42:25 +0400

    User Sign In

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getAuth } from 'firebase/auth';
 
 const Profile = () => {
-    return (
-        <div>
-            <h1>Profile</h1>
-        </div>
-    );
+    const auth = getAuth();
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        setUser(auth.currentUser);
+        console.log(auth.currentUser, 'current User');
+    }, []);
+
+    return user
+        ? (
+            <h1>{ user.displayName }</h1>
+        )
+        : (
+            <h1>Not Logged In</h1>
+       );
 };
 
 export default Profile;
