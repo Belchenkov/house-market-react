f966b81202373158a4b186ee8821d6354b6e8815
Алексей Бельченков <u608110@gmail.com>
2022-05-10 13:53:17 +0400

    Fetch Listings From Firebase

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,9 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+    collection,
+    getDocs,
+    query,
+    orderBy,
+    limit,
+    startAfter,
+    getDoc,
+    where,
+} from "firebase/firestore";
+import { toast } from "react-toastify";
+
+import { db } from "../firebase.config";
+import Spinner from "../components/Spinner";
 
 const Category = () => {
+    const [listings, setListings] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const params = useParams();
+
+    useEffect(() => {
+        const fetchListings = async () => {
+            try {
+                setLoading(true);
+
+                const listingsRef = collection(db, 'listings');
+                const q = query(
+                    listingsRef,
+                    where('type', '==', params.categoryName),
+                    orderBy('timestamp', 'desc'),
+                    limit(10)
+                );
+
+                const querySnap = await getDocs(q);
+                let listings = [];
+
+                querySnap.forEach((doc) => {
+                    return listings.push({
+                        id: doc.id,
+                        data: doc.data(),
+                    });
+                });
+
+                setLoading(false);
+                setListings(listings);
+                console.log(listings);
+            } catch (error) {
+                console.error(error);
+                toast.error('Could not fetch listings!');
+            }
+        }
+
+        fetchListings();
+    }, [])
+
     return (
-        <div>
-            Category
+        <div className='category'>
+            <header>
+                <p className="pageHeader">
+                    { params.categoryName === 'rent' ? 'Places for rent' : 'Places for sale' }
+                </p>
+            </header>
+
+            { loading
+                ? <Spinner />
+                : listings && listings.length > 0
+                    ? (<>
+                        <main>
+                            <ul className="categoryListings">
+                                {listings.map(listing => (
+                                    <h3 key={listing.id}>{ listing.data.name }</h3>
+                                ))}
+                            </ul>
+                        </main>
+                       </>)
+                    : <p>No listings for {params.categoryName}</p>
+            }
         </div>
     );
 };
