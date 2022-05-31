f1cb139e8111a2a44868c03bfc6f5fbde6657703
Алексей Бельченков <u608110@gmail.com>
2022-05-31 17:46:54 +0400

    Contact Landlord Page

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Profile from './pages/Profile';
 import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
 import Listing from './pages/Listing';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
               <Route path='/forgot-password' element={<ForgotPassword />} />
               <Route path='/create-listing' element={<CreateListing />} />
               <Route path='/category/:categoryName/:listingId' element={<Listing />} />
+              <Route path='/contact/:landlordId' element={<Contact />} />
               <Route path='/profile' element={<PrivateRoute />}>
                   <Route path='/profile' element={<Profile />} />
               </Route>
